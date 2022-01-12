3639de339af07c1853cd09ec8b3bb092bb62776e
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-01-12 13:24:09 +0600

    created a complete hand detection module

--- hand_detection/hand_detection_module.py
+++ hand_detection/hand_detection_module.py
@@ -2,34 +2,62 @@ import cv2
 import mediapipe as mp
 import time
 
-from mediapipe.python.solutions import hands
 
-cap = cv2.VideoCapture(0)
+class HandDetector:
+    def __init__(self, static_image_mode=False, max_num_hands=2, model_complexity=1, min_detection_confidence=.5, min_tracking_confidence=.5) -> None:
+        self.static_image_mode = static_image_mode,
+        self.max_num_hands = max_num_hands,
+        self.model_complexity = model_complexity,
+        self.min_detection_confidence = min_detection_confidence,
+        self.min_tracking_confidence = min_tracking_confidence
 
-mpHands = mp.solutions.hands
+        self.mpHands = mp.solutions.hands
+        self.mpDraw = mp.solutions.drawing_utils
+        self.hands = self.mpHands.Hands(
+            static_image_mode, max_num_hands, model_complexity, min_detection_confidence, min_tracking_confidence)
 
-mpDraw = mp.solutions.drawing_utils
+    def findHands(self, img, shouldDraw=False):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = self. hands.process(imgRGB)
 
-hands = mpHands.Hands()
+        # print(result.multi_hand_landmarks)
+
+        self.multi_hand_landmarks = result.multi_hand_landmarks
+        if self.multi_hand_landmarks:
+            for hand in self.multi_hand_landmarks:
+
+                if shouldDraw:
+                    self.mpDraw.draw_landmarks(
+                        img, hand, self.mpHands.HAND_CONNECTIONS)
+
+    def findhandPosition(self, img, handNumber=0, shouldDraw=False):
+        landmarkList = []
+        hgt, wdt, c = img.shape
+        if self.multi_hand_landmarks:
+            # print(self.multi_hand_landmarks)
+            if len(self.multi_hand_landmarks) >= handNumber:
+                for id, lm in enumerate(self.multi_hand_landmarks[handNumber].landmark):
+                    cx, cy = int(lm.x*wdt), int(lm.y*hgt)
+                    print(cx, cy)
+                    landmarkList.append([id, cx, cy])
+                    if shouldDraw:
+                        cv2.circle(img, (cx, cy), 15,
+                                   (255, 0, 255), cv2.FILLED)
+        return landmarkList
 
 
-while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    result = hands.process(imgRGB)
-    hgt, wdt, c = img.shape
-    # print(result.multi_hand_landmarks)
+def main():
+    cap = cv2.VideoCapture(0)
+    handDetector = HandDetector()
+    while True:
+        success, img = cap.read()
+        handDetector.findHands(img, True)
+        handDetector.findhandPosition(img)
+        # cv2.putText(img, text="abcd", org=(10, 70), color=(255, 0, 255),
+        #             fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=3)
+        cv2.imshow("image", img)
+        cv2.waitKey(1)
 
-    multi_hand_landmarks = result.multi_hand_landmarks
-    if multi_hand_landmarks:
-        for hand in multi_hand_landmarks:
-            # for id, lm in enumerate(hand.landmark):
-            #     cx, cy = int(lm.x*wdt), int(lm.y*hgt)
-            #     if id == 3:
-            #         cv2.circle(img, (cx, cy), 20, (255, 0, 255))
-            mpDraw.draw_landmarks(img, hand, mpHands.HAND_CONNECTIONS)
 
-    cv2.putText(img, text="abcd", org=(10, 70), color=(255, 0, 255),
-                fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=3)
-    cv2.imshow("image", img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    main()
